2719bbadec986754665f5bb7eb1109694b322c9a
Frog-Knight-Thomas <bakerm2978@student.faytechcc.edu>
2026-09-02 13:15:30 -0400

    Add files via upload

---
+++ p1Lab1.py
@@ -0,0 +1,10 @@
+# Michael Baker
+# 9/2/2026
+# p1lab1.py
+# input and print
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello," , first_name,last_name + "!", "Welcome to CTI-110")
